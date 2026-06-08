42e738035c7bcea95ad8e2b5c8a1e2caaa710c15
Sangeetha Subash <sansgamesolutions@gmail.com>
2026-06-08 18:08:16 -0400

    lighting added

--- Examples/GlfwStandalone/main.cxx
+++ Examples/GlfwStandalone/main.cxx
@@ -173,6 +173,10 @@ int main()
 
         app.GetCamera().FitToSphere({2.5f, 1.f, 1.f}, 3.f);
 
+        // Key light (warm, upper-right) + fill light (cool, lower-left).
+        app.GetWorld().AddLight("key",  { 6.f,  6.f,  5.f}, {1.0f, 0.95f, 0.85f}, 1.2f);
+        app.GetWorld().AddLight("fill", {-3.f, -2.f, -2.f}, {0.6f, 0.75f, 1.0f},  0.4f);
+
         // Blocking convenience path (same as old Run()):
         app.Run();
 
